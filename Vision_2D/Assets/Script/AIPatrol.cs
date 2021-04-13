8d93bed89fbfd58c6cb08fec225436b3bf72ebcb
TranquilSean <73656939+TranquilSean@users.noreply.github.com>
2021-04-13 01:21:38 +0100

    Added GUI
    AI patrol in circles
    Pit in some extra lights
    Started User Interface/HUBs

--- Vision_2D/Assets/Script/AIPatrol.cs
+++ Vision_2D/Assets/Script/AIPatrol.cs
@@ -6,15 +6,26 @@ using UnityEngine.Events;
 
 public class AIPatrol : MonoBehaviour
 {
+    const string RIGHT = "right";
+    const string LEFT = "left";
+    const string UP = "up";
+    const string DOWN = "down";
+    
     public Rigidbody2D rb;
     public Collider2D bodyCollider;
-    public Collider2D FOV;
-    public GameObject Player;
+    public Collider2D fov;
+    public GameObject player;
     public LayerMask playerLayer;
     public LayerMask wallLayer;
     public LayerMask patrolLayer;
     public float walkSpeed;
     [SerializeField] private GameManager manager;
+    string facingDirection;
+    
+    private void Start()
+    {
+        facingDirection = RIGHT;
+    }
 
     // Update is called once per frame
     void Update()
@@ -24,22 +35,59 @@ public class AIPatrol : MonoBehaviour
 
     void Patrol()
     {
-        if(bodyCollider.IsTouchingLayers(wallLayer) || bodyCollider.IsTouchingLayers(patrolLayer))
+        if (bodyCollider.IsTouchingLayers(wallLayer) || bodyCollider.IsTouchingLayers(patrolLayer))
         {
             Flip();
         }
 
-        if (FOV.IsTouchingLayers(playerLayer))
+        if (fov.IsTouchingLayers(playerLayer))
         {
             manager.RespawnPlayer();
         }
-        //Movement
-        rb.velocity = new Vector2(walkSpeed * Time.fixedDeltaTime, rb.velocity.y);
+
+        switch (facingDirection)
+        {
+            case RIGHT: 
+                rb.rotation = 0; 
+                rb.velocity = new Vector2(walkSpeed * Time.fixedDeltaTime, 0);
+                break;
+            case LEFT: 
+                rb.rotation = 180;
+                rb.velocity = new Vector2(walkSpeed * Time.fixedDeltaTime, 0);
+                break;
+            case UP: 
+                rb.rotation = 90; 
+                rb.velocity = new Vector2(0, walkSpeed * Time.fixedDeltaTime);
+                break;
+            case DOWN: 
+                rb.rotation = 270;
+                rb.velocity = new Vector2(0, walkSpeed * Time.fixedDeltaTime);
+                break;
+        }
     }
 
     void Flip()
     {
-        transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
-        walkSpeed *= -1;
+        switch (facingDirection)
+        {
+            case RIGHT:
+                facingDirection = DOWN;
+                walkSpeed *= -1;
+                break;
+            case DOWN:
+                facingDirection = LEFT;
+                break;
+            case LEFT:
+                facingDirection = UP;
+                walkSpeed *= -1;
+                break;
+            case UP:
+                facingDirection = RIGHT;
+                break;
+        }
+
+        //transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
+        //walkSpeed *= -1;
+        //transform.eulerAngles = Vector3.forward * 90;
     }
 }
