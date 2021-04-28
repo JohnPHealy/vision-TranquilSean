af04d99e5e63bbbb81807e05c284d1a05b187e68
TranquilSean <73656939+TranquilSean@users.noreply.github.com>
2021-04-28 13:36:13 +0100

    Adding Teleportation

--- Vision_2D/Assets/Script/TeleportPlayer.cs
+++ Vision_2D/Assets/Script/TeleportPlayer.cs
@@ -1,14 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TeleportPlayer : MonoBehaviour
 {
-    [SerializeField] private GameManager manager;
     public GameObject player;
-    
-    void Update()
+    public Transform teleportDestination;
+
+    private void OnTriggerStay2D(Collider2D other)
     {
-        
+        if (other.gameObject.name == player.gameObject.name)
+        {
+            player.transform.position = teleportDestination.position;
+        }
     }
 }
