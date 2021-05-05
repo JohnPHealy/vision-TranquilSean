be51748d386f4bc37952b492c7e854bd785b3f9c
TranquilSean <73656939+TranquilSean@users.noreply.github.com>
2021-05-05 23:10:58 +0100

    Added Doors/Keys
    Followed CodeMonkey Tutorial to code

--- Vision_2D/Assets/Script/GameManager.cs
+++ Vision_2D/Assets/Script/GameManager.cs
@@ -7,15 +7,29 @@ using UnityEngine.Events;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private GameObject player;
+    [SerializeField] private UnityEvent<string> addKey;
     private Vector3 startPos;
+    public static  int keyNum;
 
     private void Start()
     {
         startPos = player.transform.position;
+        keyNum = 0;
+        UpdateUI();
         //PauseGame();
 
     }
-
+    
+    public void PickupKey()
+    {
+        keyNum += 1;
+        UpdateUI();
+    }
+    private void UpdateUI()
+    {
+        addKey.Invoke(keyNum.ToString());
+        
+    }
     public void Checkpoint()
     {
         startPos = player.transform.position;
